e0b87105f207d73e93dae82f403fbcdca065aab6
alvromell <alvromell@gmail.com>
2026-04-12 21:01:21 +0200

    add Sanity CLI config

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,7 @@ import { schema } from "./src/sanity/schemaTypes";
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  basePath: '/studio',
   plugins: [
     structureTool()
   ],
